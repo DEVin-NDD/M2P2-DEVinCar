913b4754e7cbd66be4f59352364f6c34f6447777
Lucas Jesus <lucas.jesus@ndd.tech>
2022-08-16 01:39:52 -0300

    Criação de script da tabela Address

---
+++ DEVinCar.Api/Data/Scripts/script_DevInCar.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE [dbo].[Addresses] (
+    [Id]         INT           NOT NULL,
+    [CityId]     INT           NULL,
+    [Street]     VARCHAR (150) NULL,
+    [Cep]        NCHAR (8)     NULL,
+    [Number]     INT           NULL,
+    [Complement] NCHAR (255)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
