71b47ef09d50dd106f9eae8355bb30f80f38d2db
angelosartoneto <angelosartoneto@outlook.com>
2022-08-16 20:46:04 -0300

    feature: adiciona DBContext

--- DEVinCar.Api/Controllers/UserController.cs
+++ DEVinCar.Api/Controllers/UserController.cs
@@ -1,6 +1,17 @@
-﻿namespace DEVinCar.Api.Controllers
+﻿using DEVinCar.Api.Data;
+using DEVinCar.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEVinCar.Api.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
 {
-    public class UserController
+    private readonly DevInCarDbContext _context;
+
+    public UserController(DevInCarDbContext context)
     {
+        _context = context;
     }
 }
