fefa31af43540730ca12d417f869ef4f09d61f91
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-08-15 21:37:02 -0300

    feature:Criar Model Sale

---
+++ DEVinCar.Api/Models/Sale.cs
@@ -0,0 +1,21 @@
+﻿namespace DEVinCar.Api.Models
+{
+    public class Sale
+    {
+
+        public int Id { get; internal set; }
+        public int BuyerldId { get; set; }
+        public int SellerId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Sale()
+        {
+        }
+
+        public Sale(int buyerldId, int sellerId, DateTime saleDate)
+        {
+            BuyerldId = buyerldId;
+            SellerId = sellerId;
+            SaleDate = saleDate;
+        }
+    }
+}
