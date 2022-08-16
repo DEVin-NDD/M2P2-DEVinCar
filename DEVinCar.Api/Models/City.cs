810962adda14b1a7b2d147f71f0138f79bddc681
Lucas Lucindo <lucas.lucindosilva@gmail.com>
2022-08-15 22:30:55 -0300

    feature: Criar Model City

---
+++ DEVinCar.Api/Models/City.cs
@@ -0,0 +1,17 @@
+﻿namespace DEVinCar.Api.Models
+{
+    public class City
+    {
+        
+        public int Id { get; set; }
+        public int StateId { get; set; }
+        public string Name { get; set; }
+
+        public City(int id, int stateId, string name)
+        {
+            Id = id;
+            StateId = stateId;
+            Name = name;
+        }
+    }
+}
