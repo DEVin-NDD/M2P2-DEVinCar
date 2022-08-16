77450f8ec7dd9b5d90cfcd95abe94b84106aacca
angelosartoneto <angelosartoneto@outlook.com>
2022-08-15 22:13:42 -0300

    Feature: Adiciona classe DTOcar.

---
+++ DEVinCar.Api/DTOs/carDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEVinCar.Api.DTOs
+{
+    public class carDTO
+    {
+        [Required(ErrorMessage = "The name is required")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public decimal SuggestedPrice { get; set; }
+
+    }
+}
