6a45f2257eb4d10f9fc7971f41f1fa66be8e32f8
rodrigo costa da rosa <rodrigorosa.am@gmail.com>
2022-08-16 21:58:25 -0300

    feature: add ctor car

--- DEVinCar.Api/Models/Car.cs
+++ DEVinCar.Api/Models/Car.cs
@@ -5,5 +5,14 @@
         public int Id { get; internal set; }
         public string Name { get; set; }
         public decimal SuggestedPrice { get; set; }
+        public Car ()
+        {
+        }
+        public Car(int id, string name, decimal suggestedPrice)
+        {
+            Id = id;
+            Name = name;
+            SuggestedPrice = suggestedPrice;
+        }
     }
 }
