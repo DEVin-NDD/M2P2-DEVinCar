a936cac372840e4bc7a3f9a334b4b3a2b7432378
Ernesto Araújo <101370745+ernestopcaraujo@users.noreply.github.com>
2022-08-16 21:24:32 -0300

    feature: implementa metodo delete user by id.

--- DEVinCar.Api/Controllers/UserController.cs
+++ DEVinCar.Api/Controllers/UserController.cs
@@ -9,9 +9,26 @@ namespace DEVinCar.Api.Controllers;
 public class UserController : ControllerBase
 {
     private readonly DevInCarDbContext _context;
-
     public UserController(DevInCarDbContext context)
     {
         _context = context;
     }
+
+//Endpoint Delete User By Id - Ernesto Araújo
+    [HttpDelete("{userId}")]
+    public ActionResult Delete(
+        [FromRoute] int userId
+    )
+    {
+        var user = _context.Users.Find(userId);
+
+        if(user == null){ 
+            return NotFound();
+        }
+        _context.Users.Remove(user);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+    
 }
