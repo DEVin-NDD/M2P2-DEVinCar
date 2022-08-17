762506c47f324c5f0de59fa0b371fbe234cf7214
Cleiton Ricardo Spagnol <101300606+cleitonricardo@users.noreply.github.com>
2022-08-17 15:37:53 -0300

    feature: Retirado o CityId e o Required do Complement

--- DEVinCar.Api/DTOs/AdressDTO.cs
+++ DEVinCar.Api/DTOs/AdressDTO.cs
@@ -4,8 +4,6 @@ namespace DEVinCar.Api.DTOs
 {
     public class AdressDTO
     {
-        [Required(ErrorMessage = "The CityId is required")]
-        public int CityId { get; set; }
         [Required(ErrorMessage = "The Street is required")]
         [MaxLength(150,ErrorMessage="Street name must be a maximum of 100 characters")]
         public string Street { get; set; }
@@ -14,7 +12,6 @@ namespace DEVinCar.Api.DTOs
         public string Cep { get; set; }
         [Required(ErrorMessage = "The Number is required")]
         public int Number { get; set; }
-        [Required(ErrorMessage = "The Complement is required")]
         [MaxLength(255,ErrorMessage="The Complement must have a maximum of 255 characters")]
         public string Complement { get; set; }
 
