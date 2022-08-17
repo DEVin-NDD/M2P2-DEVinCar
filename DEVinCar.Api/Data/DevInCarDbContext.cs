8176eca0fac6fc8b31522af7c64b05c17a9e2e41
Elias Silva <inboxsilva1998@gmail.com>
2022-08-17 19:24:52 -0300

    Feature: Implementa mapeamento de estados

--- DEVinCar.Api/Data/DevInCarDbContext.cs
+++ DEVinCar.Api/Data/DevInCarDbContext.cs
@@ -18,6 +18,7 @@ public class DevInCarDbContext : DbContext
     public DbSet<Sale> Sales { get; set; }
     public DbSet<SaleCar> SaleCars { get; set; }
     public DbSet<Delivery> Deliveries { get; set; }
+    public DbSet<State> States { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -169,6 +170,56 @@ public class DevInCarDbContext : DbContext
             entity
                 .Property(d => d.DeliveryForecast)
                 .HasColumnType("timestamp");
+        });
+
+        modelBuilder.Entity<State>(entity =>
+        {
+            entity.ToTable("States");
+            entity.HasKey(s => s.Id);
+	        entity.Property(s => s.Id)
+                .HasColumnType("int");
+
+            entity
+                .Property(s => s.Name)
+		        .HasColumnType("varchar")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity
+                .Property(s => s.Initials);
+                .HasColumnType("varchar")
+                .HasMaxLength(2);
+
+            entity
+                .HasData(new[] {
+                    new State (1, "Acre", "AC"),
+                    new State (2, "Alagoas", "AL"),
+                    new State (3, "Amapá", "AP"),
+                    new State (4, "Amazonas", "AM"),
+                    new State (5, "Bahia", "BA"),
+                    new State (6, "Ceará", "CE"),
+                    new State (7, "Distrito Federal", "DF"),
+                    new State (8, "Espírito Santo", "ES"),
+                    new State (9, "Goiás", "GO"),
+                    new State (10, "Maranhão", "MA"),
+                    new State (11, "Mato Grosso", "MT"),
+                    new State (12, "Mato Grosso do Sul", "MS"),
+                    new State (13, "Minas Gerais", "(MG"),
+                    new State (14, "Pará", "PA"),
+                    new State (15, "Paraíba", "PB"),
+                    new State (16, "Paraná", "PR"),
+                    new State (17, "Pernambuco", "PE"),
+                    new State (18, "Piauí", "PI"),
+                    new State (19, "Rio de Janeiro", "RJ"),
+                    new State (20, "Rio Grande do Norte", "RN"),
+                    new State (21, "Rio Grande do Sul", "RS"),
+                    new State (22, "Rondônia", "RO"),
+                    new State (23, "Roraima", "RR"),
+                    new State (24, "Santa Catarina", "SC"),
+                    new State (25, "São Paulo", "SP"),
+                    new State (26, "Sergipe", "SE"),
+                    new State (27, "Tocantins", "TO")
+                });
         });
     }
 }
