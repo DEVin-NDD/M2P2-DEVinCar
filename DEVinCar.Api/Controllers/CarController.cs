b4afd2ec1688b8ce011a954e3e4b01088cfc8baf
lucas20211 <lucasrukhan@gmail.com>
2022-08-17 20:59:19 -0300

    feature: implementa metodo get

--- DEVinCar.Api/Controllers/CarController.cs
+++ DEVinCar.Api/Controllers/CarController.cs
@@ -15,4 +15,39 @@ public class CarController : ControllerBase
     {
         _context = context;
     }
+    
+    [HttpGet]
+    public ActionResult<List<Car>> Get(
+        [FromQuery] string name,
+        [FromQuery] decimal? priceMin,
+        [FromQuery] decimal? priceMax
+
+    )
+    {
+        // Regras de negócio;
+        var query = _context.Cars.AsQueryable();
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            query = query.Where(c => c.Name == name);//Retorna a lista name
+        }
+        if(priceMin > priceMax) 
+        {
+            return BadRequest();//Erro 400
+        }
+        if(priceMin.HasValue) 
+        {
+            query = query.Where(c => c.SuggestedPrice == priceMin); // Preço Minimo
+        }
+        if(priceMax.HasValue)
+        {
+            query = query.Where(c => c.SuggestedPrice == priceMax); // Preço Maximo
+        }
+        if( query == null )
+        {
+            return NoContent();
+        }
+
+        return Ok(query.ToList());
+    }
 }
