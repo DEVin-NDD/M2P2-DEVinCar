ca55798b2caec34ac712c0818896c43817d2e6d8
angelosartoneto <angelosartoneto@outlook.com>
2022-08-18 20:14:52 -0300

    feature: adiciona get-user-controller

--- DEVinCar.Api/Controllers/UserController.cs
+++ DEVinCar.Api/Controllers/UserController.cs
@@ -16,6 +16,44 @@ public class UserController : ControllerBase
         _context = context;
     }
 
+    [HttpGet]
+    public ActionResult<List<User>> Get(
+
+    [FromQuery] string Name,
+    [FromQuery] DateTime? birthDateMax,
+    [FromQuery] DateTime? birthDateMin
+)
+    {
+
+        var query = _context.Users.AsQueryable();
+
+        if (!string.IsNullOrEmpty(Name))
+        {
+            query = query.Where(c => c.Name.Contains(Name));
+        }
+
+        if (birthDateMin.HasValue)
+        {
+            query = query.Where(c => c.BirthDate >= birthDateMin.Value);
+        }
+
+        if (birthDateMax.HasValue)
+        {
+            query = query.Where(c => c.BirthDate <= birthDateMax.Value);
+        }
+
+        if (query.Count() == 0)
+        {
+            return NoContent();
+        }
+
+        return Ok(
+            query
+            .OrderBy(c => c.Name)
+            .ToList()
+            );
+    }
+
     [HttpGet("{id}")]
     public ActionResult<User> GetById(
         [FromRoute] int id
