e2f604c9e3d66d0fddb57bc4f43348eb6d81ee02
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-08-18 21:10:58 -0300

    Feacture:Criar Endpoint Post/user/userId/Sales

---
+++ DEVinCar.Api/DTOs/SaleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEVinCar.Api.DTOs
+{
+    public class SaleDTO
+    {
+        public int UserId { get; set; }//SellerId
+        [Required(ErrorMessage = "The BuyerId is required.")]
+       
+        public int BuyerId { get; set; }
+        public DateTime SaleDate { get; set; }
+    }
+}
