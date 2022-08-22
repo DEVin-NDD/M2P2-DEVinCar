3bb5ec7f5dd64954bb00e22ddbd4b4b5b9f546d3
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2022-08-22 16:07:06 -0300

    feature: implementa post address

--- DEVinCar.Api/Controllers/StatesController.cs
+++ DEVinCar.Api/Controllers/StatesController.cs
@@ -17,6 +17,37 @@ public class StatesController : ControllerBase
         _context = context;
     }
 
+    [HttpPost("{stateId}/city/{cityId}/address")]
+    public ActionResult<Address> PostAdress(
+        [FromRoute] int stateId,
+        [FromRoute] int cityId,
+        [FromBody] AdressDTO body)
+    {
+        var idState = _context.States.Find(stateId);
+        var idCity = _context.Cities.Find(cityId);
 
+        if(idState == null || idCity == null)
+        {
+            return NotFound();
+        }
+
+        if(idCity.StateId != idState.Id)
+        {
+            return BadRequest();
+        }
+
+        var address = new Address
+        {
+            CityId = cityId,
+            Street = body.Street,
+            Number = body.Number,
+            Cep = body.Cep,
+            Complement = body.Complement
+
+        };
+        _context.Addresses.Add(address);
+        _context.SaveChanges();
+        return Created("api/sale", address.Id);
+    }
 
 }
