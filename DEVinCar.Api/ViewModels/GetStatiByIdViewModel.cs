08a8998f9ebe8819d3d9262d32b5c4559e479489
dayane <dayane.barrosoliveira@gmail.com>
2022-08-24 23:37:40 -0300

    Fix:Alterado a resposta do endpoint-getStatiById

---
+++ DEVinCar.Api/ViewModels/GetStatiByIdViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DEVinCar.Api.ViewModels
+{
+    public class GetStatiByIdViewModel
+    {
+        public int StatiId { get; set; }
+        public string StatiName { get; set; }
+        public string StatiInitials { get; set; }
+
+        public GetStatiByIdViewModel(int id, string name, string initials)
+        {
+            StatiId = id;
+            StatiName = name;
+            StatiInitials = initials;
+        }
+
+    }
+}
